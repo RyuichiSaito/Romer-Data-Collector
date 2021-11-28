ec9147502c352c3f9551e9c160549b9f2c87fe9f
Ryuichi Saito <ryu185132@gmail.com>
2021-11-29 01:27:10 +0900

    add new functions

--- MFCApplication2/MFCApplication2Dlg.cpp
+++ MFCApplication2/MFCApplication2Dlg.cpp
@@ -13,6 +13,7 @@
 #include <iomanip>
 #include <shlwapi.h>
 #include <numeric>
+#include <wchar.h>
 
 #pragma comment(lib, "shlwapi.lib")
 
@@ -48,6 +49,7 @@ const int size_y = 11;
 long double Memo[size_x][size_y];
 long double Data[size_x][(size_y - 1)*2];
 long double Cur[size_xcur][size_y];
+
 
 // アプリケーションのバージョン情報に使われる CAboutDlg ダイアログ
 
@@ -88,7 +90,7 @@ END_MESSAGE_MAP()
 
 CMFCApplication2Dlg::CMFCApplication2Dlg(CWnd* pParent /*=nullptr*/)
 	: CDialogEx(IDD_MFCAPPLICATION2_DIALOG, pParent)
-	, m_strXPosition_(_T(""))
+	, m_strXPosition(_T(""))
 	, m_strYPosition(_T(""))
 	, m_strZPosition(_T(""))
 	, m_strA_angle(_T(""))
@@ -98,7 +100,15 @@ CMFCApplication2Dlg::CMFCApplication2Dlg(CWnd* pParent /*=nullptr*/)
 	, m_strE_angle(_T(""))
 	, m_strF_angle(_T(""))
 	, m_strG_angle(_T(""))
+	, m_strEvent(_T(""))
+	, m_strArmStatus(_T(""))
+	, m_strProbe(_T(""))
 	, m_bButton2State(FALSE)
+	, m_bServerAvailable(FALSE)
+	, m_bEventSyncd(FALSE)
+	, m_strXPosition_tool(_T("0"))
+	, m_strYPosition_tool(_T("0"))
+	, m_strZPosition_tool(_T("0"))
 {	
 	// Initialize RDS variables
 	// Use RDS_INITIALIZE_EXT is you've got an authentication key provided by Romer
@@ -109,7 +119,7 @@ CMFCApplication2Dlg::CMFCApplication2Dlg(CWnd* pParent /*=nullptr*/)
 void CMFCApplication2Dlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
-	DDX_Text(pDX, IDC_EDIT1, m_strXPosition_);
+	DDX_Text(pDX, IDC_EDIT1, m_strXPosition);
 	DDX_Text(pDX, IDC_EDIT2, m_strYPosition);
 	DDX_Text(pDX, IDC_EDIT3, m_strZPosition);
 	DDX_Text(pDX, IDC_EDIT4, m_strA_angle);
@@ -119,8 +129,16 @@ void CMFCApplication2Dlg::DoDataExchange(CDataExchange* pDX)
 	DDX_Text(pDX, IDC_EDIT8, m_strE_angle);
 	DDX_Text(pDX, IDC_EDIT9, m_strF_angle);
 	DDX_Text(pDX, IDC_EDIT10, m_strG_angle);
+	DDX_Text(pDX, IDC_EDIT11, m_strXPosition_tool);
+	DDX_Text(pDX, IDC_EDIT12, m_strYPosition_tool);
+	DDX_Text(pDX, IDC_EDIT13, m_strZPosition_tool);
 	DDX_Check(pDX, IDC_CHECK1, m_bButton2State);
-	//DDX_Control(pDX, IDC_BUTTON1, OnBnClickedButton1);
+	DDX_Check(pDX, IDC_CHECK2, m_bServerAvailable);
+	DDX_Check(pDX, IDC_CHECK3, m_bEventSyncd);
+
+	DDX_Text(pDX, IDC_EDIT_Event, m_strEvent);
+	DDX_Text(pDX, IDC_EDIT_ArmStatus, m_strArmStatus);
+	DDX_Text(pDX, IDC_EDIT_Probe, m_strProbe);
 }
 
 BEGIN_MESSAGE_MAP(CMFCApplication2Dlg, CDialogEx)
@@ -141,6 +159,7 @@ END_MESSAGE_MAP()
 BOOL CMFCApplication2Dlg::OnInitDialog()
 {
 	CDialogEx::OnInitDialog();
+
 
 	// "バージョン情報..." メニューをシステム メニューに追加します。
 
@@ -168,6 +187,7 @@ BOOL CMFCApplication2Dlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 小さいアイコンの設定
 
 	// TODO: 初期化をここに追加します。
+
 
 	return TRUE;  // フォーカスをコントロールに設定した場合を除き、TRUE を返します。
 }
@@ -247,9 +267,11 @@ void CMFCApplication2Dlg::OnBnClickedButton1()
 
 		// ファイル出力用のオブジェクトを作成
 		std::ofstream data_file(pathName);
+		data_file << "Button Status, X,Y,Z,A1,A2,A3,A4,A5,A6,A7,X tool,Y tool,Z tool";
 		for (int i = 0; i < iter; i++) {
 			for (int j = 0; j < size_y; j++) {
 				data_file << fixed << setprecision(15) << Memo[i][j] << ",";
+				data_file << m_strXPosition_tool << "," << m_strYPosition_tool << "," << m_strZPosition_tool << ",";
 			}
 			data_file << "\n";
 		}
@@ -261,11 +283,13 @@ void CMFCApplication2Dlg::OnBnClickedButton1()
 		std::ofstream data_file_2(filename);
 		data_file_2 << "X mean,Y mean,Z mean,A1 mean,A2 mean,A3 mean,A4 mean,A5 mean,A6 mean,A7 mean,";
 		data_file_2 << "X std,Y std,Z std,A1 std,A2 std,A3 std,A4 std,A5 std,A6 std,A7 std";
+		data_file_2 << ",X tool,Y tool,Z tool";
 		data_file_2 << "\n";
 
 		for (int i = 0; i < data_num; i++) {
 			for (int j = 0; j < (size_y-1)*2; j++) {
 				data_file_2 << fixed << setprecision(15) << Data[i][j] << ",";
+				data_file_2 << m_strXPosition_tool << "," << m_strYPosition_tool << "," << m_strZPosition_tool << ",";
 			}
 			data_file_2 << "\n";
 		}
@@ -363,7 +387,7 @@ void CMFCApplication2Dlg::OnBnClickedButton4()
 // RDS_VALID_DATA is automatically called when program receives a "valid" RDS event
 void CMFCApplication2Dlg::RDS_SYNCED_DATA(sRDSPosition& p)
 {	
-	m_strXPosition_.Format(_T("%12.4f"), p.ProbeXYZ[RDS_X]);
+	m_strXPosition.Format(_T("%12.4f"), p.ProbeXYZ[RDS_X]);
 	m_strYPosition.Format(_T("%12.4f"),p.ProbeXYZ[RDS_Y]);
 	m_strZPosition.Format(_T("%12.4f"), p.ProbeXYZ[RDS_Z]);
 
@@ -395,6 +419,7 @@ void CMFCApplication2Dlg::RDS_SYNCED_DATA(sRDSPosition& p)
 		iter++;
 	}
 
+	m_bEventSyncd = TRUE;
 	UpdateData(FALSE);
 	Sleep(10);
 }
@@ -403,7 +428,7 @@ void CMFCApplication2Dlg::RDS_SYNCED_DATA(sRDSPosition& p)
 // RDS_INVALID_DATA is automatically called when program receives an "invalid" RDS event
 void CMFCApplication2Dlg::RDS_UNSYNCED_DATA(sRDSPosition& p)
 {
-	m_strXPosition_.Format(_T("%12.4f"), p.ProbeXYZ[RDS_X]);
+	m_strXPosition.Format(_T("%12.4f"), p.ProbeXYZ[RDS_X]);
 	m_strYPosition.Format(_T("%12.4f"), p.ProbeXYZ[RDS_Y]);
 	m_strZPosition.Format(_T("%12.4f"), p.ProbeXYZ[RDS_Z]);
 
@@ -433,30 +458,54 @@ void CMFCApplication2Dlg::RDS_UNSYNCED_DATA(sRDSPosition& p)
 		iter++;
 	}
 
+	m_bEventSyncd = FALSE;
 	UpdateData(FALSE);
 	Sleep(10);
 }
+
+
+// Refresh link status
+void CMFCApplication2Dlg::OnTimer(UINT nIDEvent)
+{
+	m_bServerAvailable = m_bServerFound;
+	UpdateData(FALSE);
+
+	CDialog::OnTimer(nIDEvent);
+}
+
 
 // RDS_SERVER_UP is called when application is connected to service
 void CMFCApplication2Dlg::RDS_SERVER_UP(void)
 {
 	MessageBox(L"Server is up !");
+	//m_bServerAvailable = TRUE;
 }
 
 // RDS_SERVER_DOWN is called when application is disconnected from service
 void CMFCApplication2Dlg::RDS_SERVER_DOWN(void)
 {
 	MessageBox(L"Server is down !");
+	//m_bServerAvailable = FALSE;
 }
 
 // RDS_PROBE_DATA is automatically called when probe changes
 void CMFCApplication2Dlg::RDS_PROBE_DATA(sRDSProbe& probe)
 {
+	m_strEvent = "RDS_PROBE_DATA event";
+
+	m_strProbe.Format(_T("Index=%ld, Name=%ls, Type=%ls"),probe.Index, probe.Name, probe.TypeName);
+
+	UpdateData(FALSE);
 }
 
 // RDS_ARM_DATA is called every 500ms and gives interesting information about connection with the arm
 void CMFCApplication2Dlg::RDS_ARM_DATA(sRDSArm& arm)
 {
+	m_strEvent = "RDS_ARM_DATA event";
+
+	m_strArmStatus = arm.ArmStatusText;
+
+	UpdateData(FALSE);
 }
 
 
--- MFCApplication2/resource.h
+++ MFCApplication2/resource.h
@@ -21,7 +21,16 @@
 #define IDC_BUTTON1                     1011
 #define IDC_BUTTON2                     1012
 #define IDC_BUTTON3                     1013
+#define IDC_EDIT11                      1014
 #define IDC_BUTTON4                     1015
+#define IDC_EDIT12                      1016
+#define IDC_EDIT13                      1018
+#define IDC_CHECK2                      1019
+#define IDC_CHECK3                      1020
+#define IDC_EDIT_Event                  1021
+#define IDC_EDIT_ArmStatus              1022
+#define IDC_EDIT_Probe                  1023
+#define IDC_BUTTON5                     1024
 
 // Next default values for new objects
 // 
@@ -29,7 +38,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_CONTROL_VALUE         1018
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
